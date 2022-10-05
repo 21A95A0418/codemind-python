7ac4d70902d8b2492fe03b06b6f4a89b15415891
21A95A0418 <109515128+21A95A0418@users.noreply.github.com>
2022-10-05 20:48:28 +0530

    n = int(input()) m = list(map(int,input().split())) k = [] o = [] for i in range(n):     for x in m:         if x%2==0:             k.append(int(x))         else:             o.append(x)     break print(*(k+o))

---
+++ even_elements_followed_by_odd.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = list(map(int,input().split()))
+k = []
+o = []
+for i in range(n):
+    for x in m:
+        if x%2==0:
+            k.append(int(x))
+        else:
+            o.append(x)
+    break
+print(*(k+o))
