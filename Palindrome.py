f73ca88c2bea8c74e5646494d506172557e0dc6a
21A95A0418 <109515128+21A95A0418@users.noreply.github.com>
2022-12-28 05:58:19 +0530

    number = int(input()) list1 = [] list2 = list(str(number)) while number !=0:     last = number%10     list1.append(str(last))     number = number//10 if list1==list2:     print('True') else:     print('False')

--- Palindrome.py
+++ Palindrome.py
@@ -1,7 +1,11 @@
-n = int(input())
-m = str(n)
-s = int(m[::-1])
-if n==s:
+number = int(input())
+list1 = []
+list2 = list(str(number))
+while number !=0:
+    last = number%10
+    list1.append(str(last))
+    number = number//10
+if list1==list2:
     print('True')
 else:
     print('False')
